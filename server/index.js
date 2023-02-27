8dedd4ac9e53b6fce50459aaa804e4201c68e9a4
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-02-27 11:58:07 +0100

    build server. Next: deploy w/ Render

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -41,7 +45,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), verifyToken, register);
+app.post("/auth/register", upload.single("picture"), register); // verifyToken middleware to add
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
